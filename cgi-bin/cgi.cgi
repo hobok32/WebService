fa64a0681709590547432d6eac3a88de0ef50d60
hobok32 <53633941+hobok32@users.noreply.github.com>
2019-08-03 21:25:56 +0700

    d

--- cgi-bin/cgi.cgi
+++
@@ -1,5 +0,0 @@
-#!"C:\xampp\xampp\perl\bin\perl.exe"
-
-print "Content-Type: text/html\n\n";
-
-print "OK";
--- htdocs/DemoLive/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a='a';
-echo $a;
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
